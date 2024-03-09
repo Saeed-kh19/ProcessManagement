4518700aa2ed00cff41439ca412885045fa1db15
Saeed-kh19 <saeedkhalili719@gmail.com>
2024-03-10 02:07:30 +0330

    Base Menu

--- ProcessManagement/Program.cs
+++ ProcessManagement/Program.cs
@@ -10,11 +10,46 @@ namespace ProcessManagement
     {
         static void Main(string[] args)
         {
+            //set variables
+            int selection;
+
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Welcome to process management application!\n\n");
             Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("select an action you want to do: ");
+            Console.WriteLine("Please select an action: \n");
+            Console.WriteLine("1. Run a process");
+            Console.WriteLine("2. List processes");
+            Console.WriteLine("3. Kill a process");
+            Console.WriteLine("4. Hirearchy of a specific process");
+line1:      try
+            {
+                selection = int.Parse(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter a number! try again");
+                goto line1;
+            }
+            switch (selection)
+            {
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+                default:
+                    Console.WriteLine("Incorrect number! try again");
+                    goto line1;
+            }
             Console.ReadKey();
+
         }
     }
 }
