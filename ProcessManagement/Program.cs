d4328c5860438361318d6e97d3e5f4ac200d0927
Saeed-kh19 <saeedkhalili719@gmail.com>
2024-03-10 01:54:14 +0330

    Welcomization!

--- ProcessManagement/Program.cs
+++ ProcessManagement/Program.cs
@@ -10,6 +10,11 @@ namespace ProcessManagement
     {
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Welcome to process management application!\n\n");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("select an action you want to do: ");
+            Console.ReadKey();
         }
     }
 }
