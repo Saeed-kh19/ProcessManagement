d1b685b487b2fb2401de4f06499b317a47ef1b76
Saeed-kh19 <saeedkhalili719@gmail.com>
2024-03-11 02:00:40 +0330

    Menu re-design and adding runProcess feature!

--- ProcessManagement/Program.cs
+++ ProcessManagement/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace ProcessManagement
 {
@@ -12,36 +13,74 @@ namespace ProcessManagement
         {
             //set variables
             int selection;
+            string processToRun;
 
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Welcome to process management application!\n\n");
+        //Menu Design
+        line0: Console.Clear();
             Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("--------------------------------------------");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(" Welcome to process management application!");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("--------------------------------------------");
             Console.WriteLine("Please select an action: \n");
             Console.WriteLine("1. Run a process");
             Console.WriteLine("2. List processes");
             Console.WriteLine("3. Kill a process");
-            Console.WriteLine("4. Hirearchy of a specific process");
-line1:      try
+            Console.WriteLine("4. Hirearchy of a specific process\n");
+        line1: try
             {
+                Console.ForegroundColor = ConsoleColor.Yellow;
                 selection = int.Parse(Console.ReadLine());
+                Console.ResetColor();
             }
             catch (Exception)
             {
+                Console.ForegroundColor = ConsoleColor.Gray;
                 Console.WriteLine("Please enter a number! try again");
                 goto line1;
             }
             switch (selection)
             {
                 case 1:
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write("Type your proccess to run");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.Write(" (or press (b) to back to main menu)");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write(":\n");
+                    while (true)
+                    {
 
-                    break;
+                    line2: Console.ForegroundColor = ConsoleColor.Yellow;
+                        processToRun = Console.ReadLine();
+                        if (processToRun == "b")
+                        {
+                            goto line0;
+                        }
+                        try
+                        {
+                            runProcess(processToRun);
+                        }
+                        catch (Exception)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Incorrect command!");
+                            Console.ResetColor();
+                            goto line2;
+                        }
+                    }
                 case 2:
+                    Console.Clear();
 
                     break;
                 case 3:
+                    Console.Clear();
 
                     break;
                 case 4:
+                    Console.Clear();
 
                     break;
                 default:
@@ -49,6 +88,22 @@ line1:      try
                     goto line1;
             }
             Console.ReadKey();
+
+        }
+        static void runProcess(string process)
+        {
+            Process.Start(process);
+        }
+        static void listProcess()
+        {
+
+        }
+        static void killProcess()
+        {
+
+        }
+        static void processTree()
+        {
 
         }
     }
