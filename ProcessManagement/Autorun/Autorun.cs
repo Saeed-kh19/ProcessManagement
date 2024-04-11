227b63adbe4a925ecf71810704a2c43ca51772de
Saeed-kh19 <saeedkhalili719@gmail.com>
2024-04-11 11:58:16 +0330

    Adding visual effects to Insert Function and some edits in Main method!...

--- ProcessManagement/Autorun/Autorun.cs
+++ ProcessManagement/Autorun/Autorun.cs
@@ -11,6 +11,7 @@ class Program
 
         ManagementEventWatcher watcher = new ManagementEventWatcher();
         WqlEventQuery query = new WqlEventQuery("SELECT * FROM Win32_VolumeChangeEvent WHERE EventType = 2");
+        watcher.EventArrived += Insert;
         watcher.Query = query;
         watcher.Start();
         Console.ReadLine();
@@ -26,10 +27,19 @@ class Program
             try
             {
                 Process.Start("mspaint.exe");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write($"Flash disk ");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write($"{driveInfo.VolumeLabel}");
+                Console.ForegroundColor= ConsoleColor.Green;
+                Console.Write(" detected successfully!");
+                Console.ResetColor();
             }
             catch (Exception)
             {
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine($"An error occured!");
+                Console.ResetColor();
             }
         }
     }
