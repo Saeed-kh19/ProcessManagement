9a4fe3dd599a0698c1b34f537aedc32858ae7a7d
Saeed-kh19 <saeedkhalili719@gmail.com>
2024-04-11 11:23:00 +0330

    Welcomization and creating watcher for flash disk

---
+++ ProcessManagement/Autorun/Autorun.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Management;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please insert your flash drive:");
+
+        ManagementEventWatcher watcher = new ManagementEventWatcher();
+        WqlEventQuery query = new WqlEventQuery("SELECT * FROM Win32_VolumeChangeEvent WHERE EventType = 2");
+        watcher.Query = query;
+        watcher.Start();
+        Console.ReadLine();
+        watcher.Stop();
+    }
+
+}
+
