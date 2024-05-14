3a6f9f7c0873c43bd6d99c9f3d5b94ffda84542a
Saeed-kh19 <saeedkhalili719@gmail.com>
2024-05-14 23:15:33 +0330

    Creating Mutex program and initializations!

---
+++ ProcessManagement/Semaphors & more/MUTEX.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ProcessManagement.Semaphors___more
+{
+    internal class MUTEX
+    {
+        static void Main(string[] args)
+        {
+            Mutex mutex = new Mutex(true, "myAppMutex");
+            for (int i = 0; i < 5; i++)
+            {
+                try
+                {
+                    if (!mutex.WaitOne(TimeSpan.FromSeconds(5)) == false)
+                    {
+                        Thread.Sleep(3000);
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("Another instance of the app is running!\n");
+                        Console.ResetColor();
+                    }
+                    RunProgram();
+                }
+                catch (Exception e)
+                {
+                    if (mutex != null)
+                    {
+                        mutex.Close();
+                        mutex = null;
+                    }
+                }
+                if (i==4)
+                {
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine("Program finished! press any key to exit...");
+                    Console.ResetColor();
+                    Console.ReadKey();
+                }
+            }
+        }
+
+        static void RunProgram()
+        {
+            Thread.Sleep(3000);
+            Console.ForegroundColor= ConsoleColor.Green;
+            Console.WriteLine("Running program...\n");
+            Console.ResetColor();
+        }
+    }
+}
---
+++ ProcessManagement/Semaphors & more/lock.cs
@@ -0,0 +1,39 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading;
+//using System.Threading.Tasks;
+
+//namespace ProcessManagement.Semaphors___more
+//{
+//    internal class @lock
+//    {
+//        private readonly static object _lockObject = new object();
+
+//        public static void Process()
+//        {
+//            bool lockTaken = false;
+//            try
+//            {
+//                Monitor.Enter(_lockObject, ref lockTaken);
+
+//                // Your code here.
+//                Console.WriteLine("Locked section is running.");
+//            }
+//            finally
+//            {
+//                if (lockTaken)
+//                {
+//                    Monitor.Exit(_lockObject);
+//                }
+//            }
+//        }
+
+//        public static void Main(string[] args)
+//        {
+//            Process();
+//            Console.ReadKey();
+//        }
+//    }
+//}
