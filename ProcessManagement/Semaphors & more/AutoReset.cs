1c93e4c743d899a8803fc14f00fd1d40c877d13b
Saeed-kh19 <saeedkhalili719@gmail.com>
2024-05-16 02:29:38 +0330

    Finilizations and more!... :)

--- ProcessManagement/Semaphors & more/AutoReset.cs
+++ ProcessManagement/Semaphors & more/AutoReset.cs
@@ -1,44 +1,4 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading;
-//using System.Threading.Tasks;
-
-//namespace ProcessManagement.Semaphors___more
-//{
-//    internal class AutoReset
-//    {
-//        private static AutoResetEvent autoEvent;
-
-//        public static void Main(string[] args)
-//        {
-//            autoEvent = new AutoResetEvent(false);
-
-//            Console.WriteLine("Starting a new thread...\n");
-//            Thread t1 = new Thread(Thread1);
-//            t1.Start();
-
-//            Console.WriteLine("Waiting for Thread 2 to signal...\n");
-
-//            autoEvent.WaitOne();
-//            Console.WriteLine("Thread 1 Signaled!\n");
-
-//            Console.WriteLine("Press any key to exit...");
-//            Console.ReadKey();
-//        }
-
-//        static void Thread1()
-//        {
-//            Console.WriteLine("Thread 1 Started!\n");
-//            Thread.Sleep(2000);
-//            Console.WriteLine("Thread 1 finished , Signaling Thread 2...\n");
-//            autoEvent.Set();
-//        }
-//    }
-//}
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Threading;
 
@@ -48,8 +8,12 @@ class Program
 
     public static void Main(string[] args)
     {
+
         Console.ResetColor();
 
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine("<AutoResetEvent Exercise>\n\n");
+        Console.ResetColor();
 
 
         autoEvents = new List<AutoResetEvent>();
@@ -57,8 +21,8 @@ class Program
         for (int i = 0; i < 5; i++)
         {
             autoEvents.Add(new AutoResetEvent(false));
-            Thread workerThread = new Thread(WorkerMethod);
-            workerThread.Start(i);
+            Thread subThread = new Thread(SubThread);
+            subThread.Start(i);
         }
 
         Console.WriteLine("Waiting for all Sub-Threads to signal...\n");
@@ -68,22 +32,20 @@ class Program
 
         Console.WriteLine("All Sub-Threads signaled!\n");
 
-        Console.WriteLine("Press any key to exit...\n");
+        Console.WriteLine("\nProgram Finished! Press any key to exit...\n");
         Console.ReadKey();
     }
 
-    static void WorkerMethod(object state)
+    static void SubThread(object state)
     {
-        int workerId = (int)state;
+        int subThreadId = (int)state;
 
-        Console.WriteLine($"Sub-Thread {workerId} started!\n");
+        Console.WriteLine($"Sub-Thread {subThreadId} started!\n");
 
         Thread.Sleep(new Random().Next(1000, 5000));
 
-        Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine($"Sub-Thread {workerId} finished, signaling Main Thread...\n");
-        Console.ResetColor();
+        Console.WriteLine($"Sub-Thread {subThreadId} finished, Signaling Main Thread...\n");
 
-        autoEvents[workerId].Set();
+        autoEvents[subThreadId].Set();
     }
 }
