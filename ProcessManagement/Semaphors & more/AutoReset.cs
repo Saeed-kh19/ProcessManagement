8fe62228c79f960a713ba531e1b53be2b8555b9f
Saeed-kh19 <saeedkhalili719@gmail.com>
2024-05-16 02:47:47 +0330

    Creating & Initializing Semaphores Classes...!

--- ProcessManagement/Semaphors & more/AutoReset.cs
+++ ProcessManagement/Semaphors & more/AutoReset.cs
@@ -1,54 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Threading;
 
-class Program
-{
-    private static List<AutoResetEvent> autoEvents;
+//class Program
+//{
+//    private static List<AutoResetEvent> autoEvents;
 
-    public static void Main(string[] args)
-    {
+//    public static void Main(string[] args)
+//    {
 
-        //Welcomizations!
-        Console.ForegroundColor = ConsoleColor.Cyan;
-        Console.WriteLine("<AutoResetEvent Exercise>\n\n");
-        Console.ResetColor();
+//        //Welcomizations!
+//        Console.ForegroundColor = ConsoleColor.Cyan;
+//        Console.WriteLine("<AutoResetEvent Exercise>\n\n");
+//        Console.ResetColor();
 
-        //Creating a generic list of auto reset event
-        autoEvents = new List<AutoResetEvent>();
+//        //Creating a generic list of auto reset event
+//        autoEvents = new List<AutoResetEvent>();
 
-        //Creating and starting 5 Threads...!
-        for (int i = 0; i < 5; i++)
-        {
-            autoEvents.Add(new AutoResetEvent(false));
-            Thread subThread = new Thread(SubThread);
-            subThread.Start(i);
-        }
+//        //Creating and starting 5 Threads...!
+//        for (int i = 0; i < 5; i++)
+//        {
+//            autoEvents.Add(new AutoResetEvent(false));
+//            Thread subThread = new Thread(SubThread);
+//            subThread.Start(i);
+//        }
 
-        Console.WriteLine("Waiting for all Sub-Threads to signal...\n");
+//        Console.WriteLine("Waiting for all Sub-Threads to signal...\n");
 
-        WaitHandle.WaitAll(autoEvents.ToArray());
+//        WaitHandle.WaitAll(autoEvents.ToArray());
 
-        Console.WriteLine("All Sub-Threads signaled!\n");
+//        Console.WriteLine("All Sub-Threads signaled!\n");
 
-        //Finishing program and waiting for a key to exit the program...!
-        Console.WriteLine("\nProgram Finished! Press any key to exit...\n");
-        Console.ReadKey();
-    }
+//        //Finishing program and waiting for a key to exit the program...!
+//        Console.WriteLine("\nProgram Finished! Press any key to exit...\n");
+//        Console.ReadKey();
+//    }
 
-    //Functions for sub threads!...
-    static void SubThread(object state)
-    {
-        //Creating Id for threads (with usage of their states!)
-        int subThreadId = (int)state;
+//    //Functions for sub threads!...
+//    static void SubThread(object state)
+//    {
+//        //Creating Id for threads (with usage of their states!)
+//        int subThreadId = (int)state;
 
-        Console.WriteLine($"Sub-Thread {subThreadId} started!\n");
+//        Console.WriteLine($"Sub-Thread {subThreadId} started!\n");
 
-        //Waiting for a random time!
-        Thread.Sleep(new Random().Next(1000, 5000));
+//        //Waiting for a random time!
+//        Thread.Sleep(new Random().Next(1000, 5000));
 
-        Console.WriteLine($"Sub-Thread {subThreadId} finished, Signaling Main Thread...\n");
+//        Console.WriteLine($"Sub-Thread {subThreadId} finished, Signaling Main Thread...\n");
 
-        autoEvents[subThreadId].Set();
-    }
-}
+//        autoEvents[subThreadId].Set();
+//    }
+//}
---
+++ ProcessManagement/Semaphors & more/Semaphores.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProcessManagement.Semaphors___more
+{
+    internal class Semaphores
+    {
+        static Semaphore s = new Semaphore(2, 2);
+        public static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("<Semaphores Exercise>\n\n");
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            for (int i = 0; i < 5; i++)
+            {
+                Thread th = new Thread((Object obj) =>
+                {
+                    s.WaitOne(); //s--
+                    Console.WriteLine("Start with: " + obj.ToString() + "\n");
+                    Thread.Sleep(3000);
+                    Console.WriteLine("End with: " + obj.ToString() + "\n");
+                    s.Release(); //s++
+                });
+                th.Start(i + 1);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
