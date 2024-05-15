552e3eabe3e577f6fd7d46a5b415487e0d67dd9a
Saeed-kh19 <saeedkhalili719@gmail.com>
2024-05-16 01:41:46 +0330

    adding Welcomizations to the Mutex class...!

--- ProcessManagement/Semaphors & more/MUTEX.cs
+++ ProcessManagement/Semaphors & more/MUTEX.cs
@@ -1,63 +1,67 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using System.Threading;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
 
-//namespace ProcessManagement.Semaphors___more
-//{
-//    internal class MUTEX
-//    {
-//        static void Main(string[] args)
-//        {
-//            Mutex mutex = new Mutex(false, "Mutex");
-//            for (int i = 0; i < 5; i++)
-//            {
-//                try
-//                {
+namespace ProcessManagement.Semaphors___more
+{
+    internal class MUTEX
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("<Mutex Exercise>\n\n");
 
-//                    if (!mutex.WaitOne(TimeSpan.FromSeconds(5)) == false)
-//                    {
-//                        //Sleeping for 3 seconds!
-//                        Thread.Sleep(3000);
-//                        Console.ForegroundColor = ConsoleColor.Yellow;
-//                        Console.WriteLine("Another instance of the app is running!\n");
-//                        Console.ResetColor();
-//                    }
+            Console.ResetColor();
+            Mutex mutex = new Mutex(false, "Mutex");
+            for (int i = 0; i < 3; i++)
+            {
+                try
+                {
 
-//                    //Running program if mutex isn't taken!
-//                    RunProgram();
+                    if (!mutex.WaitOne(TimeSpan.FromSeconds(5)) == false)
+                    {
+                        //Sleeping for 3 seconds!
+                        Thread.Sleep(3000);
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine("Another instance of the app is running!\n");
+                        Console.ResetColor();
+                    }
 
-//                }
-//                catch (Exception e)
-//                {
-//                    if (mutex != null)
-//                    {
-//                        mutex.Close();
-//                        mutex = null;
-//                    }
-//                }
-//                if (i==4)
-//                {
-//                    //Exiting program!
-//                    Console.ForegroundColor = ConsoleColor.Cyan;
-//                    Console.WriteLine("Program finished! press any key to exit...");
-//                    Console.ResetColor();
-//                    Console.ReadKey();
-//                }
-//            }
-//        }
+                    //Running program if mutex isn't taken!
+                    RunProgram();
 
-//        static void RunProgram()
-//        {
-//            //Sleep for 3 seconds!
-//            Thread.Sleep(3000);
+                }
+                catch (Exception e)
+                {
+                    if (mutex != null)
+                    {
+                        mutex.Close();
+                        mutex = null;
+                    }
+                }
+                if (i == 2)
+                {
+                    //Exiting program!
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine("Program finished! press any key to exit...");
+                    Console.ResetColor();
+                    Console.ReadKey();
+                }
+            }
+        }
 
-//            //Running the program
-//            Console.ForegroundColor= ConsoleColor.Green;
-//            Console.WriteLine("Running program...\n");
-//            Console.ResetColor();
-//        }
-//    }
-//}
+        static void RunProgram()
+        {
+            //Sleep for 3 seconds!
+            Thread.Sleep(3000);
+
+            //Running the program
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Running program...\n");
+            Console.ResetColor();
+        }
+    }
+}
--- ProcessManagement/Semaphors & more/lock.cs
+++ ProcessManagement/Semaphors & more/lock.cs
@@ -1,94 +1,94 @@
-﻿using System;
-using System.Dynamic;
-using System.Threading;
+﻿//using System;
+//using System.Dynamic;
+//using System.Threading;
 
 
-public class LOCK
-{
-    public static void Main(string[] args)
-    {
-        //Welcomization
-        Console.ForegroundColor = ConsoleColor.Cyan;
-        Console.WriteLine("<Lock Exercise>\n\n");
-        Console.ResetColor();
+//public class LOCK
+//{
+//    public static void Main(string[] args)
+//    {
+//        //Welcomization
+//        Console.ForegroundColor = ConsoleColor.Cyan;
+//        Console.WriteLine("<Lock Exercise>\n\n");
+//        Console.ResetColor();
 
-        //Creating new account class!
-        Account account = new Account(3750);
+//        //Creating new account class!
+//        Account account = new Account(3750);
 
-        //Creating 10 Threads!
-        Thread[] threads = new Thread[10];
+//        //Creating 10 Threads!
+//        Thread[] threads = new Thread[10];
 
-        for (int i = 0; i < 10; i++)
-        {
-            Thread t = new Thread(() =>
-            {
-                for (int j = 0; j < 5; j++)
-                {
-                    //Withdraw some money!
-                    account.Withdraw(250);
-                }
-            });
+//        for (int i = 0; i < 10; i++)
+//        {
+//            Thread t = new Thread(() =>
+//            {
+//                for (int j = 0; j < 5; j++)
+//                {
+//                    //Withdraw some money!
+//                    account.Withdraw(250);
+//                }
+//            });
 
-            //Naming Threads!
-            t.Name = $"Thread{i + 1}";
-            threads[i] = t;
-        }
+//            //Naming Threads!
+//            t.Name = $"Thread{i + 1}";
+//            threads[i] = t;
+//        }
 
-        for (int i = 0; i < 10; i++)
-        {
-            //Starting Threads!
-            threads[i].Start();
-        }
+//        for (int i = 0; i < 10; i++)
+//        {
+//            //Starting Threads!
+//            threads[i].Start();
+//        }
 
-        //For stopping program from quiting!
-        Console.ReadKey();
-    }
-    public class Account
-    {
-        //Create new lock object
-        private object lockObject = new object();
+//        //For stopping program from quiting!
+//        Console.ReadKey();
+//    }
+//    public class Account
+//    {
+//        //Create new lock object
+//        private object lockObject = new object();
 
-        //Create new balance property!
-        public int Balance { get; private set; }
+//        //Create new balance property!
+//        public int Balance { get; private set; }
 
-        //Account Class constructor for initializing the balance
-        public Account(int initialBalance)
-        {
-            Balance = initialBalance;
-        }
+//        //Account Class constructor for initializing the balance
+//        public Account(int initialBalance)
+//        {
+//            Balance = initialBalance;
+//        }
 
-        //a Functions for withdrawing some money from Balance!
-        public void Withdraw(int amount)
-        {
-            if (Balance < amount)
-            {
-                Console.WriteLine("Insufficient Balance!\n");
-                return;
-            }
+//        //a Functions for withdrawing some money from Balance!
+//        public void Withdraw(int amount)
+//        {
+//            if (Balance < amount)
+//            {
+//                Console.WriteLine("Insufficient Balance!\n");
+//                return;
+//            }
 
-            else
-            {
-                //The main usage of the lock process!
-                lock (lockObject)
-                {
-                    if (Balance >= amount)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Yellow;
-                        Console.WriteLine("{0} is Withdrawing\n", Thread.CurrentThread.Name);
-                        Console.ResetColor();
+//            else
+//            {
+//                //The main usage of the lock process!
+//                lock (lockObject)
+//                {
+//                    if (Balance >= amount)
+//                    {
+//                        Console.ForegroundColor = ConsoleColor.Yellow;
+//                        Console.WriteLine("{0} is Withdrawing\n", Thread.CurrentThread.Name);
+//                        Console.ResetColor();
 
-                        Balance = Balance - amount; 
+//                        Balance = Balance - amount; 
 
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        Console.WriteLine("{0} Completes Witdrawal. The balance now is: {1}\n", Thread.CurrentThread.Name, Balance);
-                        Console.ResetColor();
-                    }
-                    else
-                    {
-                        Console.WriteLine("{0} Failed to withdraw due to insufficient balance. The balance now is {1}\n", Thread.CurrentThread.Name, Balance);
-                    }
-                }
-            }
-        }
-    }
-}
+//                        Console.ForegroundColor = ConsoleColor.Green;
+//                        Console.WriteLine("{0} Completes Witdrawal. The balance now is: {1}\n", Thread.CurrentThread.Name, Balance);
+//                        Console.ResetColor();
+//                    }
+//                    else
+//                    {
+//                        Console.WriteLine("{0} Failed to withdraw due to insufficient balance. The balance now is {1}\n", Thread.CurrentThread.Name, Balance);
+//                    }
+//                }
+//            }
+//        }
+//    }
+//}
