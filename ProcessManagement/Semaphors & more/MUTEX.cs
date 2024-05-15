005edf11360f1e07924fb32f078c79cfd9424a71
Saeed-kh19 <saeedkhalili719@gmail.com>
2024-05-16 02:24:56 +0330

    Creating and Initializing the AutoReset class...!

--- ProcessManagement/Semaphors & more/MUTEX.cs
+++ ProcessManagement/Semaphors & more/MUTEX.cs
@@ -1,67 +1,67 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Threading;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Threading;
 
-namespace ProcessManagement.Semaphors___more
-{
-    internal class MUTEX
-    {
-        static void Main(string[] args)
-        {
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine("<Mutex Exercise>\n\n");
+//namespace ProcessManagement.Semaphors___more
+//{
+//    internal class MUTEX
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.ForegroundColor = ConsoleColor.Cyan;
+//            Console.WriteLine("<Mutex Exercise>\n\n");
 
-            Console.ResetColor();
-            Mutex mutex = new Mutex(false, "Mutex");
-            for (int i = 0; i < 3; i++)
-            {
-                try
-                {
+//            Console.ResetColor();
+//            Mutex mutex = new Mutex(false, "Mutex");
+//            for (int i = 0; i < 3; i++)
+//            {
+//                try
+//                {
 
-                    if (!mutex.WaitOne(TimeSpan.FromSeconds(5)) == false)
-                    {
-                        //Sleeping for 3 seconds!
-                        Thread.Sleep(3000);
-                        Console.ForegroundColor = ConsoleColor.Yellow;
-                        Console.WriteLine("Another instance of the app is running!\n");
-                        Console.ResetColor();
-                    }
+//                    if (!mutex.WaitOne(TimeSpan.FromSeconds(5)) == false)
+//                    {
+//                        //Sleeping for 3 seconds!
+//                        Thread.Sleep(3000);
+//                        Console.ForegroundColor = ConsoleColor.Yellow;
+//                        Console.WriteLine("Another instance of the app is running!\n");
+//                        Console.ResetColor();
+//                    }
 
-                    //Running program if mutex isn't taken!
-                    RunProgram();
+//                    //Running program if mutex isn't taken!
+//                    RunProgram();
 
-                }
-                catch (Exception e)
-                {
-                    if (mutex != null)
-                    {
-                        mutex.Close();
-                        mutex = null;
-                    }
-                }
-                if (i == 2)
-                {
-                    //Exiting program!
-                    Console.ForegroundColor = ConsoleColor.Cyan;
-                    Console.WriteLine("Program finished! press any key to exit...");
-                    Console.ResetColor();
-                    Console.ReadKey();
-                }
-            }
-        }
+//                }
+//                catch (Exception e)
+//                {
+//                    if (mutex != null)
+//                    {
+//                        mutex.Close();
+//                        mutex = null;
+//                    }
+//                }
+//                if (i == 2)
+//                {
+//                    //Exiting program!
+//                    Console.ForegroundColor = ConsoleColor.Cyan;
+//                    Console.WriteLine("Program finished! press any key to exit...");
+//                    Console.ResetColor();
+//                    Console.ReadKey();
+//                }
+//            }
+//        }
 
-        static void RunProgram()
-        {
-            //Sleep for 3 seconds!
-            Thread.Sleep(3000);
+//        static void RunProgram()
+//        {
+//            //Sleep for 3 seconds!
+//            Thread.Sleep(3000);
 
-            //Running the program
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Running program...\n");
-            Console.ResetColor();
-        }
-    }
-}
+//            //Running the program
+//            Console.ForegroundColor = ConsoleColor.Green;
+//            Console.WriteLine("Running program...\n");
+//            Console.ResetColor();
+//        }
+//    }
+//}
