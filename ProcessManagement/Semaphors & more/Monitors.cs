e2364fea8821f02da3fe1eb02278a46dd344673f
Saeed-kh19 <saeedkhalili719@gmail.com>
2024-05-16 15:19:09 +0330

    Finilizations and addding some comments on it! :))))

--- ProcessManagement/Semaphors & more/Monitors.cs
+++ ProcessManagement/Semaphors & more/Monitors.cs
@@ -9,33 +9,47 @@ namespace ProcessManagement.Semaphors___more
 {
     internal class Monitors
     {
+        //Creating an object for our lock!
         private static object lockObject = new object();
 
         static void Main(string[] args)
         {
-            Thread thread1 = new Thread(DoWork);
-            Thread thread2 = new Thread(DoWork);
+            //Welcomizations!
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("<Monitors Exercise>\n\n");
+            Console.ResetColor();
 
-            thread1.Start();
-            thread2.Start();
+            //Creating Threads!!
+            Thread t1 = new Thread(MonFunctions);
+            Thread t2 = new Thread(MonFunctions);
 
-            thread1.Join();
-            thread2.Join();
+            //Starting Threads
+            t1.Start();
+            t2.Start();
 
+            t1.Join();
+            t2.Join();
+
+            //Exiting the program!
+            Console.ResetColor();
+            Console.WriteLine("\n\nProgram Finished! Press any key to exit...");
             Console.ReadKey();
         }
 
-        static void DoWork()
+        //a Functions for our Monitors and working with threads!
+        static void MonFunctions()
         {
+
+            //Acquiring the monitor!
             Monitor.Enter(lockObject);
             try
             {
                 Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("Thread {0} acquired the lock", Thread.CurrentThread.ManagedThreadId);
 
-                Console.ForegroundColor= ConsoleColor.Yellow;
-                Console.WriteLine("Hello this code is working into Thread {0}!",Thread.CurrentThread.ManagedThreadId);
-                Console.WriteLine("We are still in Thread {0}!",Thread.CurrentThread.ManagedThreadId);
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Hello this code is working into Thread {0}!", Thread.CurrentThread.ManagedThreadId);
+                Console.WriteLine("We are still in Thread {0}!", Thread.CurrentThread.ManagedThreadId);
 
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("Thread {0} released the lock\n", Thread.CurrentThread.ManagedThreadId);
@@ -43,10 +57,13 @@ namespace ProcessManagement.Semaphors___more
 
                 Thread.Sleep(2000);
             }
+
+            //Releasing the monitor!
             finally
             {
                 Monitor.Exit(lockObject);
             }
+
         }
     }
 }
