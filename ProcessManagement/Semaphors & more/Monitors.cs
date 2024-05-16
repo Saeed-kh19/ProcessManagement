e61ceeb1e691fd1fd2fb12392c74d78deb72f29e
Saeed-kh19 <saeedkhalili719@gmail.com>
2024-05-16 15:13:28 +0330

    Creating & Initialization of the Monitor.cs Class!

---
+++ ProcessManagement/Semaphors & more/Monitors.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProcessManagement.Semaphors___more
+{
+    internal class Monitors
+    {
+        private static object lockObject = new object();
+
+        static void Main(string[] args)
+        {
+            Thread thread1 = new Thread(DoWork);
+            Thread thread2 = new Thread(DoWork);
+
+            thread1.Start();
+            thread2.Start();
+
+            thread1.Join();
+            thread2.Join();
+
+            Console.ReadKey();
+        }
+
+        static void DoWork()
+        {
+            Monitor.Enter(lockObject);
+            try
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Thread {0} acquired the lock", Thread.CurrentThread.ManagedThreadId);
+
+                Console.ForegroundColor= ConsoleColor.Yellow;
+                Console.WriteLine("Hello this code is working into Thread {0}!",Thread.CurrentThread.ManagedThreadId);
+                Console.WriteLine("We are still in Thread {0}!",Thread.CurrentThread.ManagedThreadId);
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Thread {0} released the lock\n", Thread.CurrentThread.ManagedThreadId);
+                Console.ResetColor();
+
+                Thread.Sleep(2000);
+            }
+            finally
+            {
+                Monitor.Exit(lockObject);
+            }
+        }
+    }
+}
--- ProcessManagement/Semaphors & more/Semaphores.cs
+++ ProcessManagement/Semaphors & more/Semaphores.cs
@@ -1,50 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading;
+//using System.Threading.Tasks;
 
-namespace ProcessManagement.Semaphors___more
-{
-    internal class Semaphores
-    {
-        //Creating a variable for our semaphore (with length of 2)
-        static Semaphore s = new Semaphore(2, 2);
+//namespace ProcessManagement.Semaphors___more
+//{
+//    internal class Semaphores
+//    {
+//        //Creating a variable for our semaphore (with length of 2)
+//        static Semaphore s = new Semaphore(2, 2);
 
-        public static void Main(string[] args)
-        {
-            //Welcomizations!
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine("<Semaphores Exercise>\n\n");
-            Console.ResetColor();
+//        public static void Main(string[] args)
+//        {
+//            //Welcomizations!
+//            Console.ForegroundColor = ConsoleColor.Cyan;
+//            Console.WriteLine("<Semaphores Exercise>\n\n");
+//            Console.ResetColor();
 
-            Console.ForegroundColor = ConsoleColor.Yellow;
+//            Console.ForegroundColor = ConsoleColor.Yellow;
 
-            //Loop for creating and starting 5 threads for our simulations!
-            for (int i = 0; i < 5; i++)
-            {
-                //Creating new Threads!
-                Thread th = new Thread((Object obj) =>
-                {
-                    //Take control of Semaphore!
-                    s.WaitOne(); //s--
-                    Console.WriteLine("Start with: " + obj.ToString() + "\n");
+//            //Loop for creating and starting 5 threads for our simulations!
+//            for (int i = 0; i < 5; i++)
+//            {
+//                //Creating new Threads!
+//                Thread th = new Thread((Object obj) =>
+//                {
+//                    //Take control of Semaphore!
+//                    s.WaitOne(); //s--
+//                    Console.WriteLine("Start with: " + obj.ToString() + "\n");
 
-                    //Wait 3 seconds between our processes!
-                    Thread.Sleep(3000);
+//                    //Wait 3 seconds between our processes!
+//                    Thread.Sleep(3000);
 
-                    Console.WriteLine("End with: " + obj.ToString() + "\n");
-                    //Release semaphore!
-                    s.Release(); //s++
-                });
+//                    Console.WriteLine("End with: " + obj.ToString() + "\n");
+//                    //Release semaphore!
+//                    s.Release(); //s++
+//                });
 
-                //Starting the threads!
-                th.Start(i + 1);
-            }
+//                //Starting the threads!
+//                th.Start(i + 1);
+//            }
 
-            //Code to read a key before exitting the program!
-            Console.ReadKey();
-        }
-    }
-}
+//            //Code to read a key before exitting the program!
+//            Console.ReadKey();
+//        }
+//    }
+//}
